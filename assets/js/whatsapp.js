f22fef89abb800a255b69c9eba8001a97d485fb0
fabi <juan-antonio.fabian@alstomgroup.com>
2026-03-20 16:30:15 +0100

    First commit to test Github pages

---
+++ assets/js/whatsapp.js
@@ -0,0 +1,15 @@
+function sendToWhatsapp() {
+    //let number = "+34649362370";
+    let number = "+34650451415";
+
+    let name = document.getElementById('form_name').value;
+    let phone = document.getElementById('form_phone').value;
+    let message = document.getElementById('form_message').value;
+
+    var url = "https://wa.me/" + number + "?text="
+        + "Nombre : " + name  + "%0a"
+        + "Teléfono : " + phone  + "%0a"
+        + "Mensaje : " + message + "%0a";
+
+    window.open(url, '_blank').focus();
+}
